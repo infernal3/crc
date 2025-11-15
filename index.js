02fcdf5150dffa88215b379c00c4984e2e60cec9
infernal3 <infernal1559@gmail.com>
2025-11-15 11:29:05 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
-var el=e=>document.getElementById(e),crc_table=[],computed=0,create_table=()=>{
+var el=e=>document.getElementById(e),crc_table=[],computed=0,create_table=X=>{
     var c,n,k;
     for(n=0;n<256;n++){
         c=BigInt(n);
         for(k=0;k<8;k++){
-            if(!!(c&1n))c=0xedb88320n^(c>>1n)
+            if(!!(c&1n))c=X^(c>>1n)
             else c=c>>1n
         }
         crc_table.push(c)
     }
     computed=1
-},update_crc=(crc,str,length)=>{
+},update_crc=(crc,str,length,method)=>{
     var c=BigInt(crc),n;
-    if(!computed)create_table();
+    if(!computed)create_table(el("a").textContent);
     for(n=0;n<length;n++){
-        c=BigInt(crc_table[(c^BigInt(str.charCodeAt(n)) )&0xffn])^BigInt(c>>8n)
+        var Z;
+        if(method==1) {Z=Number(str.at(n))*16 + Number(str.at(n+1));n++;}
+        if(method==2) Z=str.charCodeAt(n);
+        c=BigInt(crc_table[(c^BigInt(Z))&0xffn])^BigInt(c>>8n)
     }
     return c
-},crc=(str,length)=>Number(update_crc(0xffffffffn,str,length)^0xffffffffn).toString(16)
-
+},crc=(str,length)=>Number(update_crc(0xffffffffn,str,length)^0xffffffffn,el("a2).value).toString(16)
+window.addEventListener("load",()=>{
+    el("c").addEventListener("click",()=>{
+        el("b").textContent=crc(el("a1").textContent,el("a1").textContent.length);
+    });
+});
